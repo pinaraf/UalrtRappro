f1b9d1704f6807ac7bb5118fe9d8e74d4fd6d344
Pierre Ducroquet <pinaraf@pinaraf.info>
2016-08-07 15:47:35 +0200

    Update translations

--- rappro_fr.ts
+++ rappro_fr.ts
@@ -2,6 +2,78 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="fr_FR">
 <context>
+    <name>AutoMatchResultDialog</name>
+    <message>
+        <location filename="automatchresultdialog.ui" line="14"/>
+        <source>Automatic match result</source>
+        <translation>Résultat de rapprochement automatique</translation>
+    </message>
+    <message>
+        <location filename="automatchresultdialog.ui" line="32"/>
+        <source>Bank statement line</source>
+        <translation>Ligne de relevé de compte</translation>
+    </message>
+    <message>
+        <location filename="automatchresultdialog.ui" line="40"/>
+        <location filename="automatchresultdialog.ui" line="60"/>
+        <location filename="automatchresultdialog.ui" line="71"/>
+        <location filename="automatchresultdialog.ui" line="91"/>
+        <source>TextLabel</source>
+        <translation>libellé</translation>
+    </message>
+    <message>
+        <location filename="automatchresultdialog.ui" line="103"/>
+        <source>Match results</source>
+        <translation>Résultats du rapprochement</translation>
+    </message>
+    <message>
+        <location filename="automatchresultdialog.ui" line="144"/>
+        <source>Match</source>
+        <translation>Valider</translation>
+    </message>
+    <message>
+        <location filename="automatchresultdialog.ui" line="151"/>
+        <source>Cancel</source>
+        <translation>Annuler</translation>
+    </message>
+</context>
+<context>
+    <name>BankAccountingLog</name>
+    <message>
+        <location filename="bankaccountinglog.ui" line="14"/>
+        <source>New bank account settings</source>
+        <translation>Paramètres du nouveau compte bancaire</translation>
+    </message>
+    <message>
+        <location filename="bankaccountinglog.ui" line="20"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Please select the accounting log corresponding to the bank account &lt;br/&gt;%1.&lt;/p&gt;&lt;p&gt;If none exist, please fill in a name.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Veuillez sélectionner le compte comptable correspondant au compte bancaire &lt;br/&gt;%1.&lt;/p&gt;&lt;p&gt;Si aucun n&apos;existe, veuillez renseigner un nom.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="bankaccountinglog.ui" line="27"/>
+        <source>Bank account name :</source>
+        <translation>Nom du compte bancaire :</translation>
+    </message>
+</context>
+<context>
+    <name>LoginDialog</name>
+    <message>
+        <location filename="logindialog.ui" line="20"/>
+        <source>Aerogest login</source>
+        <translation>Login Aerogest</translation>
+    </message>
+    <message>
+        <location filename="logindialog.ui" line="29"/>
+        <source>Login</source>
+        <translation>Identifiant</translation>
+    </message>
+    <message>
+        <location filename="logindialog.ui" line="39"/>
+        <source>Password</source>
+        <translation>Mot de passe</translation>
+    </message>
+</context>
+<context>
     <name>MainWindow</name>
     <message>
         <location filename="mainwindow.ui" line="14"/>
@@ -14,159 +86,244 @@
         <translation>Compte bancaire</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="73"/>
+        <location filename="mainwindow.ui" line="77"/>
         <source>Quit</source>
         <translation>Quitter</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="76"/>
+        <location filename="mainwindow.ui" line="80"/>
         <source>Ctrl+Q</source>
         <translation>Ctrl+Q</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="81"/>
+        <location filename="mainwindow.ui" line="85"/>
         <source>Import file</source>
         <translation>Importer un fichier</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="84"/>
+        <location filename="mainwindow.ui" line="88"/>
         <source>Ctrl+O</source>
         <translation>Ctrl+O</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="92"/>
+        <location filename="mainwindow.ui" line="96"/>
         <source>Manual match</source>
         <translation>Valider manuellement</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="95"/>
+        <location filename="mainwindow.ui" line="104"/>
+        <source>Automatic match</source>
+        <translation>Rapprochement automatique</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="107"/>
         <source>Ctrl+M</source>
         <translation>Ctrl+M</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="53"/>
-        <location filename="mainwindow.cpp" line="61"/>
-        <location filename="mainwindow.cpp" line="69"/>
-        <location filename="mainwindow.cpp" line="78"/>
+        <location filename="mainwindow.cpp" line="51"/>
+        <location filename="mainwindow.cpp" line="59"/>
+        <location filename="mainwindow.cpp" line="67"/>
+        <location filename="mainwindow.cpp" line="76"/>
         <source>Error creating database</source>
         <translation>Erreur à la création de la base de données</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="53"/>
-        <location filename="mainwindow.cpp" line="78"/>
+        <location filename="mainwindow.cpp" line="51"/>
+        <location filename="mainwindow.cpp" line="76"/>
         <source>Could not open the statement database. Please contact support.</source>
         <translation>Impossible d&apos;ouvrir la base de données interne. Contactez le support.</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="61"/>
+        <location filename="mainwindow.cpp" line="59"/>
         <source>Could not read the schema. Please contact support.</source>
         <translation>Impossible de lire le schéma. Contactez le support.</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="69"/>
+        <location filename="mainwindow.cpp" line="67"/>
         <source>Could not create the statement database. Please contact support.
 Details : %1</source>
         <translation>Impossible de créer la base de données interne. Contactez le support.
 Détails : %1</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="108"/>
+        <location filename="mainwindow.cpp" line="107"/>
         <source>Import a bank file</source>
         <translation>Importer un fichier bancaire</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="108"/>
+        <location filename="mainwindow.cpp" line="107"/>
         <source>OFX file (*.ofx)</source>
         <translation>Fichier OFX (*.ofx)</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="112"/>
+        <location filename="mainwindow.cpp" line="111"/>
         <source>Failed to open file</source>
         <translation>Impossible d&apos;ouvrir le fichier</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="112"/>
+        <location filename="mainwindow.cpp" line="111"/>
         <source>The bank file could not be opened</source>
         <translation>Le fichier de relevé bancaire n&apos;a pas pu être ouvert</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="122"/>
+        <location filename="mainwindow.cpp" line="121"/>
         <source>Failed to prepare database</source>
         <translation>Erreur à la préparation de la base de données</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="122"/>
+        <location filename="mainwindow.cpp" line="121"/>
         <source>Database error while preparing for insertion : %1</source>
         <translation>Erreur de base de données à la préparation de l&apos;insertion : %1</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="134"/>
+        <location filename="mainwindow.cpp" line="133"/>
         <source>Failed to query database</source>
         <translation>Impossible de chercher dans la base de données</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="134"/>
-        <location filename="mainwindow.cpp" line="148"/>
+        <location filename="mainwindow.cpp" line="133"/>
+        <location filename="mainwindow.cpp" line="150"/>
         <source>Database error : %1</source>
         <translation>Erreur de base de données : %1</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="141"/>
-        <source>Bank account name</source>
-        <translation>Nom du compte bancaire</translation>
+        <location filename="mainwindow.cpp" line="261"/>
+        <source>Invalid bank account</source>
+        <translation>Compte bancaire invalide</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="141"/>
+        <location filename="mainwindow.cpp" line="261"/>
+        <source>No automatic match possible with this bank account, it has no corresponding accounting log.</source>
+        <translation>Impossible de faire un rapprochement automatique sur ce compte bancaire, il n&apos;a pas de compte associé en comptabilité.</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="295"/>
+        <location filename="mainwindow.cpp" line="313"/>
+        <source>Database error</source>
+        <translation>Erreur de base de données</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="295"/>
+        <source>Failed to search the Aerogest base : %1</source>
+        <translation>Impossible de rechercher la base Aerogest : %1</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="313"/>
+        <source>Failed to update the Aerogest base : %1</source>
+        <translation>Impossible de mettre à jour la base Aerogest : %1</translation>
+    </message>
+    <message>
+        <source>Bank account name</source>
+        <translation type="vanished">Nom du compte bancaire</translation>
+    </message>
+    <message>
         <source>New bank account found.
 Please give a name for the account with IBAN %1</source>
-        <translation>Nouveau compte bancaire trouvé. Veuillez donner un nom pour le compte bancaire avec l&apos;IBAN %1</translation>
+        <translation type="vanished">Nouveau compte bancaire trouvé. Veuillez donner un nom pour le compte bancaire avec l&apos;IBAN %1</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="148"/>
+        <location filename="mainwindow.cpp" line="150"/>
         <source>Failed to insert account</source>
         <translation>Impossible d&apos;insérer le compte</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="177"/>
+        <location filename="mainwindow.cpp" line="179"/>
         <source>Failed to insert line</source>
         <translation>Impossible d&apos;insérer la ligne</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="177"/>
+        <location filename="mainwindow.cpp" line="179"/>
         <source>Database error : %1 (%2)</source>
         <translation>Erreur de base de données : %1 (%2)</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="188"/>
+        <location filename="mainwindow.cpp" line="190"/>
         <source>Failed to commit database</source>
         <translation>Impossible de sauvegarder la base de données</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="188"/>
+        <location filename="mainwindow.cpp" line="190"/>
         <source>Database error while commiting : %1</source>
         <translation>Erreur de base de données à la sauvegarde : %1</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="192"/>
+        <location filename="mainwindow.cpp" line="194"/>
         <source>Parse result</source>
         <translation>Résultat de l&apos;analyze</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="192"/>
+        <location filename="mainwindow.cpp" line="194"/>
         <source>Parsed %1 accounts from this file, for %2 statement lines
 Imported %3 lines (%4 already found)</source>
         <translation>%1 comptes trouvés dans ce fichiers pour %2 lignes de relevé.
 %3 lignes importées (%4 déjà présentes)</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="231"/>
+        <location filename="mainwindow.cpp" line="236"/>
+        <location filename="mainwindow.cpp" line="250"/>
         <source>Missing selection</source>
         <translation>Sélection manquante</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="231"/>
+        <location filename="mainwindow.cpp" line="236"/>
+        <location filename="mainwindow.cpp" line="250"/>
         <source>Please select statement lines to match first.</source>
         <translation>Veuillez sélectionner les lignes de comptes à valider.</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="main.cpp" line="29"/>
+        <source>Path of aerogest database</source>
+        <translation>Emplacement de la base aerogest</translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="32"/>
+        <source>Aerogest password</source>
+        <translation>Mot de passe de la base Aerogest</translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="32"/>
+        <source>Password of the aerogest database</source>
+        <translation>Mot de passe de la base de données Aerogest</translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="48"/>
+        <source>Failed to connect to aerogest</source>
+        <translation>Impossible de se connecter à aerogest</translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="48"/>
+        <source>Unable to connect to aerogest : %1</source>
+        <translation>Impossible de se connecter à Aerogest : %1</translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="64"/>
+        <source>Database error</source>
+        <translation>Erreur de base de données</translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="64"/>
+        <source>Error while searching aerogest database : %1</source>
+        <translation>Erreur lors de la recherche dans la base de données Aerogest : %1</translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="68"/>
+        <location filename="main.cpp" line="72"/>
+        <source>Invalid login</source>
+        <translation>Identifiant incorrect</translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="68"/>
+        <source>Unknown user/password</source>
+        <translation>Nom d&apos;utilisateur/mot de passe inconnu</translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="72"/>
+        <source>Your user is not allowed</source>
+        <translation>Cet utilisateur n&apos;est pas autorisé</translation>
     </message>
 </context>
 </TS>
